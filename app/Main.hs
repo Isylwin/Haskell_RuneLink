f97ecfec3d2bdbdd10137d43d43de1ad0354a0a3
Oscar de Leeuw <Oscardeleeuw@hotmail.com>
2020-04-26 17:52:14 +0200

    Yeeaaaa actual code commit..

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,42 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+module Main(main) where
 
-import qualified GI.Gtk as Gtk
-import Data.GI.Base
+import Graphics.Gloss
+
+runeHoleRadius = 22.5
+runeHoleOffset = 5
+runeHoleXAmount = 7
+runeHoleYAmount = 6
+runeHoleDistance = runeHoleOffset + runeHoleRadius * 2
+
+boardSizeX = boardSize runeHoleXAmount
+boardSizeY = boardSize runeHoleYAmount
+
+background =            makeColorI 72 62 51 255     -- RuneLink greyish background
+runelinkBoardColor =    makeColorI 108 79 44 255    -- RuneLink brown
+
+boardSize :: Float -> Float
+boardSize x = runeHoleOffset + runeHoleDistance * x
+
+window :: Display
+window = InWindow "Better RuneLink" (700, 600) (500, 500)
+
+runelinkBoard :: Picture
+runelinkBoard = color runelinkBoardColor $ rectangleSolid boardSizeX boardSizeY
+
+runeHole :: Point -> Picture
+runeHole (x,y) = translate x y $ circleSolid runeHoleRadius
+
+runeHoleLocs :: Float -> [Float]
+runeHoleLocs n = 
+    let translate = (-) $ (boardSize n) / 2
+        offset = runeHoleRadius + runeHoleOffset
+    in map (translate . (+offset) . (*runeHoleDistance)) [0..n-1]
+
+runeHoles :: [Picture]
+runeHoles = map runeHole $ (,) <$> (runeHoleLocs runeHoleXAmount) <*> (runeHoleLocs runeHoleYAmount)
+
+drawing :: Picture
+drawing = pictures $ runelinkBoard : runeHoles
 
 main :: IO ()
-main = do
-  Gtk.init Nothing
-
-  win <- new Gtk.Window [ #title := "Hi there" ]
-
-  on win #destroy Gtk.mainQuit
-
-  button <- new Gtk.Button [ #label := "Click me" ]
-
-  on button #clicked (set button [ #sensitive := False,
-                                   #label := "Thanks for clicking me" ])
-
-  #add win button
-
-  #showAll win
-
-  Gtk.main
+main = display window background drawing
